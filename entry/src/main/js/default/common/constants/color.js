c40ca70b6747af008a857883c5a34168fa3eb917
zhuozengsi <zhuozengsi@huawei.com>
2021-12-24 13:21:20 +0800

    Add Contacts app sources
    Signed-off-by: zhuozengsi <zhuozengsi@huawei.com>

--- entry/src/main/js/default/common/constants/color.js
+++ entry/src/main/js/default/common/constants/color.js
@@ -1,7 +1,17 @@
 /**
- * @file 颜色常量
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the 'License');
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http:// www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an 'AS IS' BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 export default {
 	// 头像背景颜色
 	Color: ['#FF53804D', '#FF406380', '#FF807240', '#FF664D80', '#FF8C465F', '#FF8C4F3F'],
