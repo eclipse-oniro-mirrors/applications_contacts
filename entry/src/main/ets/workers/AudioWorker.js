19fbb71f96285d2f07018123d9f8af331ce71c4d
ohos-lsw <lishiwei6@huawei.com>
2023-01-06 19:01:51 +0800

    update contact code
    Signed-off-by: ohos-lsw <lishiwei6@huawei.com>

--- entry/src/main/ets/workers/AudioWorker.js
+++ entry/src/main/ets/workers/AudioWorker.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import worker from '@ohos.worker';
 import media from '@ohos.multimedia.media';
 import Log from "@ohos.hilog";
@@ -22,6 +37,7 @@ parentPort.onmessage = function (e) {
                 });
             }
             let fileName = data.data;
+            console.log(`fileName is ${JSON.stringify(fileName)}`)
             if (mAudioPath == '') {
                 audioPlayer.fdSrc = fileName;
             } else if (mAudioPath == fileName) {
