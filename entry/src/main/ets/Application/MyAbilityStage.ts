f21ab2feffe25f328f79279afff894f7d077e2b4
18518779689 <litingting84@huawei.com>
2023-03-23 16:13:51 +0800

    up to master
    Signed-off-by: 18518779689 <litingting84@huawei.com>

--- entry/src/main/ets/Application/MyAbilityStage.ts
+++ entry/src/main/ets/Application/MyAbilityStage.ts
@@ -1,10 +1,24 @@
 import { HiLog } from '../../../../../common/src/main/ets/util/HiLog';
 import AbilityStage from "@ohos.app.ability.AbilityStage"
+import notification from '@ohos.notificationManager';
 
 const TAG = 'ContactListItemView ';
 
 export default class MyAbilityStage extends AbilityStage {
     onCreate() {
         HiLog.i(TAG, 'AbilityStage onCreate');
+        notification.setNotificationEnable({
+            bundle: "com.ohos.contacts"
+        }, true, (err, data) => {
+            if (err) {
+                HiLog.e(TAG, "enableNotification err: " + JSON.stringify(err));
+            }
+        })
+        notification.addSlot({
+            type: notification.SlotType.SOCIAL_COMMUNICATION,
+            level: notification.SlotLevel.LEVEL_HIGH,
+            desc: "missedCall",
+            lockscreenVisibility: 2
+        })
     }
 }
